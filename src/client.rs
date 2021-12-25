5b4c1c17d8fff1e14b4ded7673c850f28535e311
era <the@communist.dev>
2021-12-25 17:27:29 +0000

    Moving current command to a subcommand on the client

--- src/client.rs
+++ src/client.rs
@@ -32,85 +32,89 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     CONFIG.set(config);
 
-    //TODO subcommand for creating/updating and for disabling job
     let matches = App::new("dcron_client")
         .version("0.0.1")
         .author("Elias Granja <me@elias.sh>")
-        .about("Sets up a script to be run at a DCRON instance")
-        .arg(
-            Arg::with_name("time")
-                .short("t")
-                .long("time")
-                .value_name("CRON_SYNTAX")
-                .help("Sets the frequence you want the job to be run")
-                .required(true)
-                .index(1)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("timeout")
-                .short("o")
-                .long("timeout")
-                .value_name("TIMEOUT")
-                .help("Defines the timeout for the job, if zero no timeout will be set")
-                .index(2)
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("type")
-                .short("p")
-                .long("type")
-                .value_name("TYPE")
-                .takes_value(true)
-                .index(3)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("update")
-                .short("e")
-                .long("update")
-                .takes_value(false)
-                .index(6),
-        )
-        .arg(
-            Arg::with_name("script")
-                .short("s")
-                .long("script")
-                .takes_value(true)
-                .index(4)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("name")
-                .short("n")
-                .long("name")
-                .takes_value(true)
-                .index(5)
-                .required(true),
+        .subcommand(
+            SubCommand::with_name("create")
+                .about("Sets up a script to be run at a DCRON instance")
+                .arg(
+                    Arg::with_name("time")
+                        .short("t")
+                        .long("time")
+                        .value_name("CRON_SYNTAX")
+                        .help("Sets the frequence you want the job to be run")
+                        .required(true)
+                        .index(1)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("timeout")
+                        .short("o")
+                        .long("timeout")
+                        .value_name("TIMEOUT")
+                        .help("Defines the timeout for the job, if zero no timeout will be set")
+                        .index(2)
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("type")
+                        .short("p")
+                        .long("type")
+                        .value_name("TYPE")
+                        .takes_value(true)
+                        .index(3)
+                        .required(true),
+                )
+                .arg(
+                    //TODO not great that the name of the commannd is create but we have an update flag
+                    Arg::with_name("update")
+                        .short("e")
+                        .long("update")
+                        .takes_value(false)
+                        .index(6),
+                )
+                .arg(
+                    Arg::with_name("script")
+                        .short("s")
+                        .long("script")
+                        .takes_value(true)
+                        .index(4)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("name")
+                        .short("n")
+                        .long("name")
+                        .takes_value(true)
+                        .index(5)
+                        .required(true),
+                ),
         )
         .get_matches();
 
-    let file = Path::new(matches.value_of("script").unwrap());
-    upload_file(&file).await?;
+    if let Some(matches) = matches.subcommand_matches("create") {
+        let file = Path::new(matches.value_of("script").unwrap());
+        upload_file(&file).await?;
 
-    let mut client = PublicClient::connect("http://[::1]:50051").await?; //TODO: change with ENV variables
+        let mut client = PublicClient::connect("http://[::1]:50051").await?; //TODO: change with ENV variables
 
-    let file = upload_file(file).await?;
+        let file = upload_file(file).await?;
 
-    let request = tonic::Request::new(JobRequest {
-        name: matches.value_of("name").unwrap().into(),
-        time: matches.value_of("time").unwrap().into(),
-        location: file,
-        timeout: <i32 as FromStr>::from_str(matches.value_of("timeout").unwrap()).unwrap(),
-        update_if_exists: matches.is_present("update"),
-        job_type: job_type(matches.value_of("type").unwrap()),
-    });
+        let request = tonic::Request::new(JobRequest {
+            name: matches.value_of("name").unwrap().into(),
+            time: matches.value_of("time").unwrap().into(),
+            location: file,
+            timeout: <i32 as FromStr>::from_str(matches.value_of("timeout").unwrap()).unwrap(),
+            update_if_exists: matches.is_present("update"),
+            job_type: job_type(matches.value_of("type").unwrap()),
+        });
 
-    let response = client.new_job(request).await?;
+        let response = client.new_job(request).await?;
 
-    println!("RESPONSE={:?}", response);
-
+        println!("RESPONSE={:?}", response);
+    }
     Ok(())
 }
 
