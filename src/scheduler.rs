369ec5558fe13ba401c0760b4aa0e0f4989750a8
era <the@communist.dev>
2021-12-26 12:29:21 +0000

    small changes on api comments

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,11 +1,11 @@
-use crate::{db, job::Job};
+use crate::{config::Config, db, job::Job};
 use std::collections::HashMap;
 // job_scheduler crate https://docs.rs/job_scheduler/1.2.1/job_scheduler/
 use anyhow;
 use job_scheduler;
 use std::sync::{Arc, RwLock};
 
-// This probably should be another binary
+// Maybe should use an Arc on the Scheduler itself
 pub struct Scheduler {
     // Holds the main Job struct
     jobs: Arc<RwLock<HashMap<String, Job>>>,
@@ -21,19 +21,33 @@ impl Scheduler {
     // when updating the jobs, we need to hold a write lock
     // the job thread should request read lock, and send the job to a worker
 
-    pub fn new() -> Result<Scheduler, anyhow::Error> {
+    pub async fn new() -> Result<Scheduler, anyhow::Error> {
+        // Gets all the jobs from the database and set jobs
+        // Creates the new object
         return Err(anyhow::anyhow!("Opss"));
     }
 
     pub fn schedule_all(mut self: &Self) -> Result<(), anyhow::Error> {
+        // Get read lock
+        // Schedule all the jobs and setup jobs_id
+        // meant to be run once when we start the scheduler
         return Ok(());
     }
 
     pub fn update_schedules(mut self: &Self) -> Result<(), anyhow::Error> {
+        // main method
+        // loop
+        // every 5 minutes, goes to the database
+        // Acquires writer lock and unschedule any job that is needed and deletes from job_ids
+        // Acquires writer lock and updates jobs.
+        // schedule any new job
         return Ok(());
     }
 
-    fn run_job(job: &Job) -> Result<(), anyhow::Error> {
+    fn run_job(mut self: &Self, job: &Job) -> Result<(), anyhow::Error> {
+        // Acquires reader lock on job ids (not sure if we really need)
+        // If standalone runs the script
+        // Otherwise gets a worker IP and sends an execution request to it
         Ok(())
     }
 }
