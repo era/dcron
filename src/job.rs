56f06089e7782056c23c60cb65c98a03c0ceb3db
era <the@communist.dev>
2021-12-26 22:54:40 +0000

    Using closure! to help the lifecycle issues

--- src/job.rs
+++ src/job.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Job {
     pub name: String,
     pub time: String,
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -6,68 +6,63 @@ use crate::{
 use std::collections::HashMap;
 // job_scheduler crate https://docs.rs/job_scheduler/1.2.1/job_scheduler/
 use anyhow;
+use closure::closure;
 use job_scheduler::{self, Schedule};
 use serde::{Deserialize, Serialize};
+use std::rc::Rc;
 use std::sync::{Arc, RwLock};
 
 // Maybe should use an Arc on the Scheduler itself
-pub struct Scheduler {
+pub struct Scheduler<'a> {
     // Holds the main Job struct
-    jobs: Arc<RwLock<HashMap<String, Job>>>,
+    jobs: HashMap<String, Job>,
     // Used to unschedule a job if needed
-    job_ids: Arc<RwLock<HashMap<String, job_scheduler::Uuid>>>,
-
+    job_ids: HashMap<String, job_scheduler::Uuid>,
     // Used to request to the database only jobs created after it
     last_updated_at: i64,
+    job_scheduler: job_scheduler::JobScheduler<'a>,
 }
 
-impl Scheduler {
-    // Get all the jobs in the database and updates it every 5 min
-    // Schedule the jobs using job_scheduler and keeps their uuid
-    // when updating the jobs, we need to hold a write lock
-    // the job thread should request read lock, and send the job to a worker
+// Get all the jobs in the database and updates it every 5 min
+// Schedule the jobs using job_scheduler and keeps their uuid
+// when updating the jobs, we need to hold a write lock
+// the job thread should request read lock, and send the job to a worker
 
-    pub async fn new() -> Result<Scheduler, anyhow::Error> {
-        // Gets all the jobs from the database and set jobs
-        // Creates the new object
-        return Err(anyhow::anyhow!("Opss"));
-    }
+pub async fn new() -> Result<(), anyhow::Error> {
+    // Gets all the jobs from the database and set jobs
+    // Creates the new object
+    return Err(anyhow::anyhow!("Opss"));
+}
 
-    pub fn schedule_all(self: Self) -> Result<(), anyhow::Error> {
-        // Get read lock
-        // Schedule all the jobs and setup jobs_id
-        // meant to be run once when we start the scheduler
-        let mut sched = job_scheduler::JobScheduler::new(); //TODO add to ONCE_CELL
+pub fn schedule_all(scheduler: Arc<RwLock<Scheduler>>) -> Result<(), anyhow::Error> {
+    // Get read lock
+    // Schedule all the jobs and setup jobs_id
+    // meant to be run once when we start the scheduler
+    if let Ok(mut scheduler) = scheduler.write() {
+        let jobs = (*scheduler).jobs.clone();
+        for (job_name, job) in jobs {
+            let job_time = &job.time;
+            let copy = Rc::new((&job_name).clone());
 
-        if let Ok(ids) = self.job_ids.write() {
-            if let Ok(jobs) = self.jobs.read() {
-                for (job_name, job) in &*jobs {
-                    let copy = (&job_name).clone();
-                    let job_time = &job.time;
-
-                    //TODO Finish figthing with lifetimes
-                    let job_id = sched.add(job_scheduler::Job::new(
-                        //job_time.to_string().clone().parse().unwrap(),
-                        "* * * * 1".parse().unwrap(),
-                        move || {
-                            run_job("name".to_string());
-                        },
-                    ));
-                }
-            }
+            let job_id = (*scheduler).job_scheduler.add(job_scheduler::Job::new(
+                job_time.parse().unwrap(),
+                closure!(clone copy, || {
+                    run_job(copy.to_string());
+                }),
+            ));
         }
-        return Ok(());
     }
+    return Ok(());
+}
 
-    pub fn update_schedules(mut self: &Self) -> Result<(), anyhow::Error> {
-        // main method
-        // loop
-        // every 5 minutes, goes to the database
-        // Acquires writer lock and unschedule any job that is needed and deletes from job_ids
-        // Acquires writer lock and updates jobs.
-        // schedule any new job
-        return Ok(());
-    }
+pub fn update_schedules(scheduler: Arc<RwLock<Scheduler>>) -> Result<(), anyhow::Error> {
+    // main method
+    // loop
+    // every 5 minutes, goes to the database
+    // Acquires writer lock and unschedule any job that is needed and deletes from job_ids
+    // Acquires writer lock and updates jobs.
+    // schedule any new job
+    return Ok(());
 }
 
 fn run_job(name: String) -> Result<(), anyhow::Error> {
