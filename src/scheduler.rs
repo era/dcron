d7c4861c7fe784d925ac3c0b418c9b78c018ef03
era <the@communist.dev>
2021-12-26 11:49:09 +0000

    Uses Locks on the structs that need it

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,14 +1,16 @@
 use crate::{db, job::Job};
 use std::collections::HashMap;
 // job_scheduler crate https://docs.rs/job_scheduler/1.2.1/job_scheduler/
+use anyhow;
 use job_scheduler;
+use std::sync::{Arc, RwLock};
 
 // This probably should be another binary
 pub struct Scheduler {
     // Holds the main Job struct
-    jobs: HashMap<String, Job>,
+    jobs: Arc<RwLock<HashMap<String, Job>>>,
     // Used to unschedule a job if needed
-    job_ids: HashMap<String, job_scheduler::Uuid>,
+    job_ids: Arc<RwLock<HashMap<String, job_scheduler::Uuid>>>,
     // Used to request to the database only jobs created after it
     last_updated_at: i64,
 }
@@ -18,4 +20,7 @@ impl Scheduler {
     // Schedule the jobs using job_scheduler and keeps their uuid
     // when updating the jobs, we need to hold a write lock
     // the job thread should request read lock, and send the job to a worker
+    fn schedule_all(self: &Self) -> Result<(), anyhow::Error> {
+        return Ok(());
+    }
 }
