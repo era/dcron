c7e8d9d85e4013de12eaca29bdc7f57ddf00166b
era <the@communist.dev>
2022-04-28 09:37:21 +0100

    add Debug trait for config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 use std::fs;
-#[derive(Deserialize, Clone)]
+use std::fmt;
+#[derive(Deserialize, Clone, Debug)]
 pub struct Config {
     pub database: Option<Database>,
     pub minio: Option<Minio>,
@@ -18,6 +19,26 @@ pub struct Minio {
     pub username: String,
     pub password: String,
     pub host: String,
+}
+
+impl fmt::Debug for Database {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Database")
+         .field("username", &self.username)
+         .field("password", &format!("*****"))
+         .field("cluster_url", &self.cluster_url)
+         .finish()
+    }
+}
+
+impl fmt::Debug for Minio {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Minio")
+         .field("username", &self.username)
+         .field("password", &format!("*****"))
+         .field("host", &self.host)
+         .finish()
+    }
 }
 
 impl Config {
